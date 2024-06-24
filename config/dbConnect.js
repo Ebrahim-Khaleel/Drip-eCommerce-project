dcb212c4f5f09eee94ff6df5e4177ba9177a5d77
EKhaleel46 <ebrahimkhaleel02@gmail.com>
2024-06-24 14:20:08 +0530

    small change

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
 module.exports = function dbconnect(){
-    return mongoose.connect('mongodb://localhost:27017/DripLuxe')
+    return mongoose.connect('mongodb+srv://ebrahimkhaleel:-XM52Q3dY2GX-k@cluster0.vh282rp.mongodb.net/')
+    
 }
